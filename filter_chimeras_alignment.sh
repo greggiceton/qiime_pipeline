0d647ee421290a1cf0a405ceee8526d1742c04f7
greggiceton <gregg@iceton.org>
2013-08-28 14:06:21 +0100

    Update filter_chimeras_alignment.sh
    Enable filtering of chimeric sequences

--- filter_chimeras_alignment.sh
+++ filter_chimeras_alignment.sh
@@ -8,3 +8,4 @@
 #PBS -o $HOME/qsub_error_files
 #PBS -e $HOME/qsub_error_files
 #
+filter_fasta.py -f $HOME/"$name"/otus/pynast_aligned_seqs/rep_set_aligned.fasta -o $HOME/$name/otus/non_chimeric_rep_set_aligned.fasta -s $HOME/$name/otus/chimeric_seqs.txt -n
