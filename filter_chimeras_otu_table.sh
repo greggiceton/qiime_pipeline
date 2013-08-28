f823a888698b09c3565e2b65889e87325c7be1fc
greggiceton <gregg@iceton.org>
2013-08-28 14:30:58 +0100

    Update filter_chimeras_otu_table.sh
    Enable filtering of chimeric OTUs from final OTU table produced by pick_open_reference_otus

--- filter_chimeras_otu_table.sh
+++ filter_chimeras_otu_table.sh
@@ -8,3 +8,4 @@
 #PBS -o $HOME/qsub_error_files
 #PBS -e $HOME/qsub_error_files
 #
+filter_otus_from_otu_table.py -i $HOME/$name/otus/otu_table_mc2_w_tax_no_pynast_failures.biom -o $HOME/$name/otus/otu_table_non_chimeric.biom -e $HOME/$name/otus/chimeric_seqs.txt
