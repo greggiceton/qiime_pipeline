91e99ddf8d5d06c54611f591b1ed25f8a158331c
greggiceton <gregg@iceton.org>
2013-08-28 17:20:16 +0100

    Update generic_submit.sh
    Got it working

--- generic_submit.sh
+++ generic_submit.sh
@@ -3,13 +3,18 @@
 #
 echo -e "\nPlease enter a 10 character job name\n"
 read name
+if [ "${#name}" -ge "11" ]; then
+        echo -e "Error - job name must be no more than 10 characters\n"
+        exit
+fi
 echo -e "\nDo you wish to be emailed job updates? Please type Yes if so\n"
 read email_updates
-if [$email_updates = Yes] then
+if [ "$email_updates" = "Yes" ]; then
   echo -e "\nPlease enter your email address\n"
   read email_address
-  final_options = "-m abe -M $email_address"
+  final_options="-m abe -M $email_address"
 fi
 echo -e "\nPlease enter your command\n"
 read cmd
-qsub -N "$name" "$final_options" "cmd"
+final=$(qsub -N $name $final_options $cmd)
+echo $final
