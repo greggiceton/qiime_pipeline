ff91f71327fff3e21cd7c34c75791a4c6a66b1fe
greggiceton <gregg@iceton.org>
2013-08-28 16:38:23 +0100

    Create generic_submit.sh

---
+++ generic_submit.sh
@@ -0,0 +1,15 @@
+#!/bash
+#Script to generically submit a task to the queue
+#
+echo -e "\nPlease enter a 10 character job name\n"
+read name
+echo -e "\nDo you wish to be emailed job updates? Please type Yes if so\n"
+read email_updates
+if [$email_updates = Yes] then
+  echo -e "\nPlease enter your email address\n"
+  read email_address
+  final_options = "-m abe -M $email_address"
+fi
+echo -e "\nPlease enter your command\n"
+read cmd
+qsub -N "$name" "$final_options" "cmd"
