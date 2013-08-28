b512aefb1c5e6bfe39e03b08771cf5470d11087e
greggiceton <gregg@iceton.org>
2013-08-28 11:30:04 +0100

    Update identify_chimeras.sh
    Adjust jobs to start = 4 to test whether hyperthreading slows things down

--- identify_chimeras.sh
+++ identify_chimeras.sh
@@ -9,4 +9,4 @@
 #PBS -e $HOME/qsub_error_files
 #
 #
-parallel_identify_chimeric_seqs.py -i $HOME/"$name"/split_out/seqs.fna -a /share/apps/qiime/core_set_aligned.fasta.imputed -m ChimeraSlayer -O 16 -X "7_$name" -o $HOME/$name/otus/chimeras
+parallel_identify_chimeric_seqs.py -i $HOME/"$name"/split_out/seqs.fna -a -O 4 /share/apps/qiime/core_set_aligned.fasta.imputed -m ChimeraSlayer -O 16 -X "7_$name" -o $HOME/$name/otus/chimeras
