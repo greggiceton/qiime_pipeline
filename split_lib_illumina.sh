e5ca13bf16ef125e834237d2329a99c43c38c78e
greggiceton <gregg@iceton.org>
2013-08-30 19:16:11 +0100

    Update split_lib_illumina.sh

--- split_lib_illumina.sh
+++ split_lib_illumina.sh
@@ -8,4 +8,4 @@
 #PBS -o $HOME/qsub_error_files
 #PBS -e $HOME/qsub_error_files
 #
-	split_libraries.py -m $HOME/$name/"$name"map.txt -f $HOME/$name/$name.fasta -M 100 -b golay -o $HOME/$name/split_out
+	split_libraries.py -m $HOME/$name/"$name"map.txt -f $HOME/$name/$name.fasta -M 100 -b golay_12 -o $HOME/$name/split_out
