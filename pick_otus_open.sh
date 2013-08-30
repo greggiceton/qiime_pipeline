d8d2c018d6dbd682d9f54544d1172a3aabedd32a
greggiceton <gregg@iceton.org>
2013-08-30 10:15:54 +0100

    Update pick_otus_open.sh
    Added suppress tree building as this is unnecessary since we will have to rebuild it anyway after chimera removal

--- pick_otus_open.sh
+++ pick_otus_open.sh
@@ -8,4 +8,4 @@
 #PBS -o $HOME/qsub_error_files
 #PBS -e $HOME/qsub_error_files
 #
-	 pick_open_reference_otus.py -i $HOME/"$name"/split_out/seqs.fna -p $HOME/$name/"$name"_qiime_params.txt -o $HOME/$name/otus -r /share/apps/qiime/gg/rep_set/97_otus.fasta -a
+	 pick_open_reference_otus.py --suppress_align_and_tree -i $HOME/"$name"/split_out/seqs.fna -p $HOME/$name/"$name"_qiime_params.txt -o $HOME/$name/otus -r /share/apps/qiime/gg/rep_set/97_otus.fasta -a
