e0b10e63cbca0b10edda1e008719158abafd9c63
greggiceton <gregg@iceton.org>
2013-08-30 17:22:02 +0100

    Create split_lib_illumina.sh

---
+++ split_lib_illumina.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Gregg Iceton
+#
+#PBS -M gregg@iceton.org
+#PBS -m abe
+#PBS -j oe
+#PBS -o $HOME/qsub_error_files
+#PBS -e $HOME/qsub_error_files
+#
+	split_libraries.py -m $HOME/$name/"$name"map.txt -f $HOME/$name/$name.fna -M 100 -b golay -o $HOME/$name/split_out
