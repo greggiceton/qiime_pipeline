e061d74e31a9b8dd5ac6da61561d28fa83b9ea3d
greggiceton <gregg@iceton.org>
2013-08-30 18:48:48 +0100

    Update filter_pynast_failures.sh

--- filter_pynast_failures.sh
+++ filter_pynast_failures.sh
@@ -8,4 +8,4 @@
 #PBS -o $HOME/qsub_error_files
 #PBS -e $HOME/qsub_error_files
 #
-filter_otus_from_otu_table.py -o $HOME/$name/otus/otu_table_mc2_w_tax_no_pynast_failures.biom -i $HOME/$name/otus/otu_table_mc2_w_tax.biom -e -i $HOME/$name/otus/pynast_aligned_seqs/rep_set_failures.fasta
+filter_otus_from_otu_table.py -o $HOME/$name/otus/otu_table_mc2_w_tax_no_pynast_failures.biom -i $HOME/$name/otus/otu_table_mc2_w_tax.biom -e $HOME/$name/otus/pynast_aligned_seqs/rep_set_failures.fasta
