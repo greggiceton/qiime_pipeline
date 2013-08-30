25da2ddaa4119ee0b671c0d70425073a3e5a9ab4
greggiceton <gregg@iceton.org>
2013-08-30 10:27:51 +0100

    Create filter_alignment.sh

---
+++ filter_alignment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Gregg Iceton
+#
+#PBS -M gregg@iceton.org
+#PBS -m abe
+#PBS -j oe
+#PBS -e $HOME/qsub_error_files
+#PBS -o $HOME/qsub_error_files
+  filter_alignment.py -o $HOME/$name/otus/pynast_aligned_seqs -i $HOME/$name/otus/pynast_aligned_seqs/rep_set_aligned.fasta
