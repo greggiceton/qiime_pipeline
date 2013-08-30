921e7c8b47a2ae23f1d447989f552efd73ffdcb7
greggiceton <gregg@iceton.org>
2013-08-30 10:24:35 +0100

    Create align.sh

---
+++ align.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Gregg Iceton
+#
+#PBS -M gregg@iceton.org
+#PBS -m abe
+#PBS -j oe
+#PBS -e $HOME/qsub_error_files
+#PBS -o $HOME/qsub_error_files
+  parallel_align_seqs_pynast.py -i $HOME/$name/otus/rep_set.fna -o $HOME/$name/otus/pynast_aligned_seqs -T
