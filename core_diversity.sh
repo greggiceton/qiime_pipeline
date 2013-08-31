58762e51e3951649c30c24d09cd4991159fae5f1
greggiceton <gregg@iceton.org>
2013-08-31 22:17:36 +0100

    Update core_diversity.sh

--- core_diversity.sh
+++ core_diversity.sh
@@ -7,5 +7,7 @@
 #PBS -j oe
 #PBS -e $HOME/qsub_error_files
 #PBS -o $HOME/qsub_error_files
-
-	core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth -c $cat
+if [ $cat = "" ] then;
+	core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth
+	else core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth -c $cat
+fi
