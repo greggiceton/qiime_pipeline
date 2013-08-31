4d79fdad6d5f243649442823f6dd2bba86449482
greggiceton <gregg@iceton.org>
2013-08-31 22:31:14 +0100

    Update core_diversity.sh

--- core_diversity.sh
+++ core_diversity.sh
@@ -7,8 +7,8 @@
 #PBS -j oe
 #PBS -e $HOME/qsub_error_files
 #PBS -o $HOME/qsub_error_files
-if [ $cat = "" ] then;
-	"core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth"
+if [ $cat = "" ]; then
+	core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth
 	else 
-	"core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth -c $cat"
+	core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth -c $cat
 fi
