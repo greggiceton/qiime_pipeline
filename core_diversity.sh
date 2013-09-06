a2a85edaccc8c963d4e568863deaeeea1a0cbb21
greggiceton <gregg@iceton.org>
2013-09-06 01:43:43 +0100

    Update core_diversity.sh

--- core_diversity.sh
+++ core_diversity.sh
@@ -7,6 +7,7 @@
 #PBS -j oe
 #PBS -e $HOME/qsub_error_files
 #PBS -o $HOME/qsub_error_files
+echo "test $cat $name $depth"
 if [ -z $cat ]; then
 	core_diversity_analyses.py -i $HOME/$name/otus/otu_table_non_chimeric.biom -o $HOME/$name/core_diversity -m $HOME/$name/"$name"map.txt -p $HOME/$name/"$name"_qiime_params.txt -a -t $HOME/$name/otus/non_chimeric.tre -e $depth
 	else 
