a433b138ddb2a50bcaedec09080aebb050e45e7b
greggiceton <gregg@iceton.org>
2013-09-06 01:40:56 +0100

    Update run_core_div.sh

--- run_core_div.sh
+++ run_core_div.sh
@@ -4,7 +4,7 @@ read namein
 print_biom_table_summary.py -i $HOME/$namein/otus/otu_table_non_chimeric.biom
 echo -e "\nPlease review the above output and decide on sampling depth (minimum number of reads to use)\n"
 read depthin
-head -n 1 $HOME/$namein/"$name"map.txt
+head -n 1 $HOME/$namein/"$namein"map.txt
 echo -e "\nPlease review the above categories and enter a comma separated list of those which you wish to compare\n"
 read catin
 RUN=$(name=$namein depth=$depthin cat=$catin qsub -N "12_$namein" -v name,depth,cat /share/apps/qiime_pipeline/core_diversity.sh)
